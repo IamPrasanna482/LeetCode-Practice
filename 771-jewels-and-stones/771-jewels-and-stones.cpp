1e612d35a9b9bd02f93571b9db3b08dc0fd2708e
Prasanna Agnihotri <prasannagnihotri24@gmail.com>
2022-02-08 23:41:23 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (77.66%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.cpp
+++ 771-jewels-and-stones/771-jewels-and-stones.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-    //using sets
-        set<char> s;
-        for(auto it:jewels){
-            s.insert(it);
-        }
-        // for(auto it:s){
-        //     cout<<it<<" ";
-        // }
         
         int ans=0;
-        for(auto it:stones){
-            if(s.count(it)>0)
-            {
-                ans++;
-            }        
+        for(int i=0;i<jewels.length();i++){
+            for(int j=0;j<stones.length();j++){
+                if(jewels[i]==stones[j]) ans++;
+            }
         }
         return ans;
+    //using sets
+//         set<char> s;
+//         for(auto it:jewels){
+//             s.insert(it);
+//         }
+//         // for(auto it:s){
+//         //     cout<<it<<" ";
+//         // }
+        
+//         int ans=0;
+//         for(auto it:stones){
+//             if(s.count(it)>0)
+//             {
+//                 ans++;
+//             }        
+//         }
+//         return ans;
     }
     
 };
