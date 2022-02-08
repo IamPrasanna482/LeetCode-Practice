9a4b2519b9f9f66595cc3174adecc8dd108ff6d9
Prasanna Agnihotri <prasannagnihotri24@gmail.com>
2022-02-08 14:46:34 +0530

    Time: 8 ms (8.74%), Space: 6 MB (25.37%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,36 +1,8 @@
 class Solution {
-    
-private:
-    int sum(int n){
-        int s=0;
-        while(n){
-            s+=n%10;
-            n/=10;
-        }
-        return s;
-    }
-    
-    int no_of_digits(int n){
-        
-        int d=0;
-        while(n){
-            d++;
-            n/=10;
-        }
-        return d;
-    }
 public:
     int addDigits(int num) {
-//         if(num==0) return 0;
-//     while(no_of_digits(sum(num))!=1){
-//         num=sum(num);
-//     }
-//         return sum(num);
-        
-        //optimized approach
         if(num==0) return 0;
         if(num%9==0) return 9;
-        return num%9;
-    
+    return num%9;
     }
 };
