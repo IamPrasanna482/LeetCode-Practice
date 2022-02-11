29d725ebba1a0fe229b95ca7808448f7c6f94895
Prasanna Agnihotri <prasannagnihotri24@gmail.com>
2022-02-11 20:40:53 +0530

    Time: 3 ms (97.94%), Space: 14.1 MB (5.26%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,37 +1,41 @@
 class Solution {
-public:
-    vector<int> searchRange(vector<int>& nums, int target) {
+private:
+    int findOcc(vector<int> v,int target, int toggle){
         int start=0;
-        int end=nums.size()-1;
-        int fo=-1;
-        int lo=-1;
+        int end=v.size()-1;
+        int x=-1;
+     
+        int mid;
         vector<int> ans;
         while(start<=end){
-            int mid=start+(end-start)/2;
-            if(nums[mid]==target){
-                fo=mid;
-                end=mid-1;
+             mid=start+(end-start)/2;
+            if(v[mid]==target){
+                x=mid;
+                if(toggle==-1) {
+                end=mid+toggle;
+                }
+                if(toggle==1){
+                start=mid+toggle;
+                }
             }
-            else if(nums[mid]>target){
-                end=mid-1;
-            }
-            else start=mid+1;
-}
-        start=0;
-        end=nums.size()-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(nums[mid]==target){
-                lo=mid;
-                start=mid+1;
-            }
-            else if(nums[mid]>target){
+            else if(v[mid]>target){
                 end=mid-1;
             }
             else start=mid+1;
 }
-        ans.push_back(fo);
-        ans.push_back(lo);
+        return x;
+    }
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans;
+        
+        int firstOccurence=findOcc(nums,target,-1); //toggle is -1 for first occurence
+        int lastOccurence=findOcc(nums,target,1);   //toggle is +1 for first occurence
+        
+        ans.push_back(firstOccurence);
+        ans.push_back(lastOccurence);
+         
         return ans;
+        
     }
 };
