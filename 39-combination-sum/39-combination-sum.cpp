0a6d808de91886668ae11309eb6c947cfcdc2ffe
Prasanna Agnihotri <prasannagnihotri24@gmail.com>
2022-04-17 16:16:11 +0530

    Time: 8 ms (73.32%), Space: 10.9 MB (71.99%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    void printCombinations(int ind,int target, vector<int> &arr,vector<vector<int>> &ans, vector<int> &ds){
+        if(ind==arr.size()){
+            if(target==0){
+                ans.push_back(ds);
+            }
+            return;
+        }
+        
+        //pick 
+        if(arr[ind]<=target){
+            ds.push_back(arr[ind]);
+            printCombinations(ind,target-arr[ind],arr,ans,ds);
+            ds.pop_back();
+        }
+        printCombinations(ind+1,target,arr,ans,ds);
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        
+        printCombinations(0,target,candidates,ans,ds);
+        return ans;
+        
+    }
+};
