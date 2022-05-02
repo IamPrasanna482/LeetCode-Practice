38e079247d59f36e396c7fddcfbc2a563128815b
Prasanna Agnihotri <prasannagnihotri24@gmail.com>
2022-05-02 12:38:32 +0530

    Time: 14 ms (51.99%), Space: 16.5 MB (13.75%) - LeetHub

---
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        vector<int> ans;
+        for(auto it:nums){
+            if(it%2==0){
+                //even
+                ans.push_back(it);
+            }
+        }
+        for(auto it:nums){
+        if(it%2==1){
+                //odd
+                ans.push_back(it);
+            }
+        }
+    return ans;    
+}
+};
