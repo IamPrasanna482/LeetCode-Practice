e903822d95f6e6ad95e51dde7bbb0a233d22a444
Prasanna Agnihotri <prasannagnihotri24@gmail.com>
2022-05-27 15:51:32 +0530

    Time: 116 ms (58.82%), Space: 58.9 MB (82.15%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -1,43 +1,24 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-//       int n=height.size();
-//       int i=0;
-//       int j=n-1;
-//       int maxWater=0;
-//         while(i<j){
-//             int h=j-i;
-//             int w=min(height[i],height[j]);
-//             // int area=w;
-//             // maxWater=max(maxWater,area);
-//             if(height[i]<height[j]){
-//                 i++;
-//             }
-//             else if(height[i]>height[j]){
-//                 j++;
-//             }
-//             else{
-//                 i++,j--;
-//             }
+        int left_highest=0, right_highest=0;
+        int i=0;
+        int j=height.size()-1;
+        int ans=0;
+        while(i<j){
+            int length=j-i;
+            int breadth=min(height[i],height[j]);
+            int maxWater=length*breadth;
+            ans=max(ans,maxWater);
             
-//          }
-        
-//         return maxWater;
-        int left = 0;
-        int right = height.size() - 1;
-        int maxi = 0;
-        while(left < right){
-            int w = right - left;
-            int h = min(height[left], height[right]);
-            int area = h * w;
-            maxi = max(maxi, area);
-            if(height[left] < height[right]) left++;
-            else if(height[left] > height[right]) right--;
+            if(height[i] < height[j]) i++;
+            else if(height[i] > height[j]) j--;
             else {
-                left++;
-                right--;
+                i++;
+                j--;
             }
+            
         }
-        return maxi;
+        return ans;
     }
 };
